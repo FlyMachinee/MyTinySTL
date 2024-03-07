7ea8fe5706bba0351fae98be0439bcf7f942427c
Other <1427632504@qq.com>
2024-03-07 17:51:36 +0800

    添加is_fundamental

--- MyTinySTL/type_traits.hpp
+++ MyTinySTL/type_traits.hpp
@@ -1396,6 +1396,36 @@ namespace __MY_NAMESPACE {
 
 	#pragma endregion
 
+	// checks if a type is a fundamental type
+	// 检查是否是基础类型
+	#pragma region is_fundamental
+	/**
+	 * @brief checks if a type is a fundamental type
+	 * @brief 检查类型是否为基础类型
+	 * @brief 包含成员 value, 表示其判断结果
+	 * 
+	 * @tparam T 需要进行判断的类型
+	*/
+	template <typename T>
+	struct is_fundamental: integral_constant<
+		bool,
+		is_arithmetic<T>::value ||
+		is_same<typename remove_cv<T>::type, ::std::nullptr_t>::value ||
+		is_void<T>::value
+	> {};
+
+	#if __HAS_CPP17
+	/**
+	 * @brief checks if a type is a fundamental type
+	 * @brief 检查类型是否为基础类型
+	 * @brief 其本身即表示判断结果
+	 * 
+	 * @tparam T 需要进行判断的类型
+	*/
+	template <typename T>
+	inline constexpr bool is_fundamental_v = is_fundamental<T>::value;
+	#endif // __HAS_CPP17
+	#pragma endregion
 
 	
 	// checks if a type is a pointer to a non-static member function or object
