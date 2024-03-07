f318757bf776588eb216d6e7f9fb3de041b46f5d
Other <1427632504@qq.com>
2024-03-07 23:15:38 +0800

    完善复合类型分类(composite type categorites)

--- MyTinySTL/type_traits.hpp
+++ MyTinySTL/type_traits.hpp
@@ -1358,12 +1358,13 @@ namespace __MY_NAMESPACE {
 
 
 
-	// ======================================================
+	// =========================================================================
 	//
 	//  Composite type categories and judges in compile-time
 	//  复合类型分类的编译期内判断
+	//  类型框图 https://upload.cppreference.com/mwiki/images/9/96/cpp_types.svg
 	//
-	// ======================================================
+	// =========================================================================
 
 	// checks if a type is an arithmetic type
 	// 检查类型是否为算术类型
@@ -1456,6 +1457,126 @@ namespace __MY_NAMESPACE {
 	inline constexpr bool is_member_pointer_v = is_member_pointer<T>::value;
 	#endif // __HAS_CPP17
 	#pragma endregion is_member_pointer
+
+	// checks if a type is a scalar type
+	// 检查类型是否为标量类型
+	#pragma region is_scalar
+	/**
+	 * @brief checks if a type is a scalar type
+	 * @brief 检查类型是否为标量类型
+	 * @brief 包含成员 value, 表示其判断结果
+	 * 
+	 * @tparam T 需要进行判断的类型
+	*/
+	template <typename T>
+	struct is_scalar: integral_constant<
+		bool, 
+		is_arithmetic<T>::value ||
+		is_same<typename remove_cv<T>::type, ::std::nullptr_t>::value ||
+		is_enum<T>::value ||
+		is_member_pointer<T>::value ||
+		is_pointer<T>::value
+	> {};
+
+	#if __HAS_CPP17
+	/**
+	 * @brief checks if a type is a scalar type
+	 * @brief 检查类型是否为标量类型
+	 * @brief 其本身即表示判断结果
+	 * 
+	 * @tparam T 需要进行判断的类型
+	*/
+	template <typename T>
+	inline constexpr bool is_scalar_v = is_scalar<T>::value;
+	#endif // __HAS_CPP17
+	#pragma endregion is_scalar
+
+	// checks if a type is an object type
+	// 检查是否是对象类型
+	#pragma region is_object
+	/**
+	 * @brief checks if a type is an object type
+	 * @brief 检查类型是否为对象类型
+	 * @brief 包含成员 value, 表示其判断结果
+	 * 
+	 * @tparam T 需要进行判断的类型
+	*/
+	template <typename T>
+	struct is_object: integral_constant<
+		bool, 
+		is_scalar<T>::value ||
+		is_array<T>::value ||
+		__is_class_or_union<T>::value
+	> {};
+
+	#if __HAS_CPP17
+	/**
+	 * @brief checks if a type is an object type
+	 * @brief 检查类型是否为对象类型
+	 * @brief 其本身即表示判断结果
+	 * 
+	 * @tparam T 需要进行判断的类型
+	*/
+	template <typename T>
+	inline constexpr bool is_object_v = is_object<T>::value;
+	#endif // __HAS_CPP17
+	#pragma endregion is_object
+
+	// checks if a type is a compound type
+	// 检查是否为复合类型
+	#pragma region is_compound
+	/**
+	 * @brief checks if a type is a compound type
+	 * @brief 检查是否为复合类型
+	 * @brief 其本身即表示判断结果
+	 * 
+	 * @tparam T 需要进行判断的类型
+	*/
+	template <typename T>
+	struct is_compound: integral_constant<bool, !is_fundamental<T>::value> {};
+
+	#if __HAS_CPP17
+	/**
+	 * @brief checks if a type is a compound type
+	 * @brief 检查是否为复合类型
+	 * @brief 包含成员 value, 表示其判断结果
+	 * 
+	 * @tparam T 需要进行判断的类型
+	*/
+	template <typename T>
+	inline constexpr bool is_compound_v = is_compound<T>::value;
+	#endif // __HAS_CPP17
+	#pragma endregion is_compound
+
+	// checks if a type is either an lvalue reference or rvalue reference
+	// 检查类型是否为左值引用或右值引用
+	#pragma region is_reference
+	/**
+	 * @brief checks if a type is either an lvalue reference or rvalue reference
+	 * @brief 检查类型是否为左值引用或右值引用
+	 * @brief 其本身即表示判断结果
+	 * 
+	 * @tparam T 需要进行判断的类型
+	*/
+	template <typename T>
+	struct is_reference: integral_constant<
+		bool, 
+		is_lvalue_reference<T>::value ||
+		is_rvalue_reference<T>::value
+	> {};
+
+	#if __HAS_CPP17
+	/**
+	 * @brief checks if a type is either an lvalue reference or rvalue reference
+	 * @brief 检查类型是否为左值引用或右值引用
+	 * @brief 其本身即表示判断结果
+	 * 
+	 * @tparam T 需要进行判断的类型
+	*/
+	template <typename T>
+	inline constexpr bool is_reference_v = is_reference<T>::value;
+	#endif // __HAS_CPP17
+	#pragma endregion is_reference
 
 
 
