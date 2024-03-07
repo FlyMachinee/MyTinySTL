9658e6e7c85f4fa255abb3abd8fb1d93805c58d7
Other <1427632504@qq.com>
2024-03-07 17:21:51 +0800

    添加is_member_object_pointer

--- MyTinySTL/type_traits.hpp
+++ MyTinySTL/type_traits.hpp
@@ -1277,5 +1277,38 @@ namespace __MY_NAMESPACE {
 	#endif // __HAS_CPP17
 
 	#pragma endregion is_member_function_pointer
+
+	// checks if a type is a pointer to a non-static member object
+	// 检查类型是否为指向非静态成员对象的指针
+	#pragma region is_member_object_pointer
+	template <typename T>
+	struct is_member_pointer;
+
+	/**
+	 * @brief checks if a type is a pointer to a non-static member object
+	 * @brief 检查类型是否为指向非静态成员对象的指针
+	 * @brief 包含成员 value, 表示其判断结果
+	 * 
+	 * @tparam T 需要进行判断的类型
+	*/
+	template <typename T>
+	struct is_member_object_pointer: integral_constant<
+		bool,
+		is_member_pointer<T>::value &&
+		!is_member_function_pointer<T>::value
+	> {};
+
+	#if __HAS_CPP17
+	/**
+	 * @brief checks if a type is a pointer to a non-static member object
+	 * @brief 检查类型是否为指向非静态成员对象的指针
+	 * @brief 其本身即表示判断结果
+	 * 
+	 * @tparam T 需要进行判断的类型
+	*/
+	template <typename T>
+	inline constexpr bool is_member_object_pointer_v = is_member_object_pointer<T>::type;
+	#endif // __HAS_CPP17
+	#pragma endregion is_member_object_pointer
 } // namespace __MY_NAMESPACE
 #endif // __HAS_CPP11
