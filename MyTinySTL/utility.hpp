79b5ad7df9eff3b0096f207ffee19041ad5b5326
Other <1427632504@qq.com>
2024-03-11 20:38:16 +0800

    添加my::rel_pos中的运算符函数

--- MyTinySTL/utility.hpp
+++ MyTinySTL/utility.hpp
@@ -1,4 +1,72 @@
 ﻿#pragma once
 #include "core.h"
 
-#include "type_traits.hpp"
+#if __HAS_CPP11
+#include <initializer_list>
+#endif // __HAS_CPP11
+
+#if __HAS_CPP20
+// #include "compare.hpp"
+#endif // __HAS_CPP20
+
+namespace __MY_NAMESPACE {
+
+	namespace 
+		#if __HAS_CPP20
+		[[deprecated("As of C++20, my::rel_ops are deprecated in favor of operator<=>.")]] 
+		#endif // __HAS_CPP20
+	rel_ops {
+
+		template <typename T>
+		/**
+		 * @brief 使用 operator== 实现 operator!=
+		 * @tparam T 参与比较的类型
+		 * @param lhs 比较中的左操作数
+		 * @param rhs 比较中的右操作数
+		 * @return lhs != rhs 的值，更确切地，!(lhs == rhs)
+		*/
+		__NODISCARD bool operator!=(const T& lhs, const T& rhs) {
+			return !(lhs == rhs);
+		}
+
+		template <typename T>
+		/**
+		 * @brief 使用 operator< 实现 operator>
+		 * @tparam T 参与比较的类型
+		 * @param lhs 比较中的左操作数
+		 * @param rhs 比较中的右操作数
+		 * @return lhs > rhs 的值，更确切地，rhs < lhs
+		*/
+		__NODISCARD bool operator>(const T& lhs, const T& rhs) {
+			return rhs < lhs;
+		}
+
+		template <typename T>
+		/**
+		 * @brief 使用 operator< 实现 operator<=
+		 * @tparam T 参与比较的类型
+		 * @param lhs 比较中的左操作数
+		 * @param rhs 比较中的右操作数
+		 * @return lhs <= rhs 的值，更确切地，!(rhs < lhs)
+		*/
+		__NODISCARD bool operator<=(const T& lhs, const T& rhs) {
+			return !(rhs < lhs);
+		}
+
+		template <typename T>
+		/**
+		 * @brief 使用 operator< 实现 operator>=
+		 * @tparam T 参与比较的类型
+		 * @param lhs 比较中的左操作数
+		 * @param rhs 比较中的右操作数
+		 * @return lhs >= rhs 的值，更确切地，!(lhs < rhs)
+		*/
+		__NODISCARD bool operator>=(const T& lhs, const T& rhs) {
+			return !(lhs < rhs);
+		}
+
+	} // namespace __MY_NAMESPACE::rel_ops
+	
+
+
+} // namespace __MY_NAMESPACE
