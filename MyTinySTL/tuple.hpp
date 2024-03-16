ce73bdf34e6888e4a8c52f9c4b19cd8cbbc71ede
Other <1427632504@qq.com>
2024-03-16 10:18:55 +0800

    新建tuple文件

---
+++ MyTinySTL/tuple.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "core.h"
+
+#if __HAS_CPP11
+#include <initializer_list>
+#include "type_traits.hpp"
+
+#if __HAS_CPP20
+// #include "compare.hpp"
+#endif // __HAS_CPP20
+
+namespace __MY_NAMESPACE {
+
+
+
+} // namespace __MY_NAMESPACE
+#endif // __HAS_CPP11
